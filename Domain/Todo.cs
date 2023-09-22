707c9849ed426348ba7777a478971443991b6229
oumar969 <oammar198@gmail.com>
2023-09-22 11:03:11 +0200

    TrytoCommit

---
+++ Domain/Todo.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public User Owner { get;  }
+    public string Title { get; }
+    public bool IsCompleted { get; }
+
+    public Todo(User owner, string title)
+    {
+        Owner = owner;
+        Title = title;
+    }
+    
+}
---
+++ Domain/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public List<Todo> Todos { get; set; }
+}
