b53d74fd0749cc8c79ac26b313cd609ed34c6c19
oumar969 <114076085+oumar969@users.noreply.github.com>
2023-10-09 22:17:00 +0200

    Add User Test

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,6 +3,8 @@ using Application.Logic;
 using Application.LogicInterfaces;
 using FileData;
 using FileData.DAO_s;
+using HttpClients.ClientInterfaces;
+using HttpClients.Implementations;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +22,9 @@ builder.Services.AddScoped<IUserLogic, UserLogic>();
 builder.Services.AddScoped<ITodoDao, TodoFileDao>();
 builder.Services.AddScoped<ITodoLogic, TodoLogic>();
 
-builder.Services.AddScoped(
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+
+builder.Services.AddScoped(  
     sp => 
         new HttpClient { 
             BaseAddress = new Uri("https://localhost:7093") 
