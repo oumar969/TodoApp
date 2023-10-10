4a23223f59cf0d94f3e54b15da287ae5ae25d4f2
oumar969 <oammar198@gmail.com>
2023-10-10 21:08:32 +0200

    View Todos With Filtering

--- HttpClients/ClientInterfaces/ITodoService.cs
+++ HttpClients/ClientInterfaces/ITodoService.cs
@@ -1,8 +1,15 @@
-﻿using Domain.DTOs;
+﻿using Domain;
+using Domain.DTOs;
 
 namespace HttpClients.ClientInterfaces;
 
 public interface ITodoService
 {
     Task CreateAsync(TodoCreationDto dto);
+    Task<ICollection<Todo>> GetAsync(
+        string? userName, 
+        int? userId, 
+        bool? completedStatus, 
+        string? titleContains
+    );
 }
