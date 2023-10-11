9ca825e0aa7762a8253fc6b31c58e8d063f322cf
oumar969 <oammar198@gmail.com>
2023-10-11 20:59:13 +0200

    Making a Checkbox Component

--- HttpClients/Implementations/TodoHttpClient.cs
+++ HttpClients/Implementations/TodoHttpClient.cs
@@ -23,10 +23,11 @@ public class TodoHttpClient : ITodoService
             throw new Exception(result);
         }
     }
-
     public async Task<ICollection<Todo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains)
     {
-        HttpResponseMessage response = await client.GetAsync("/todos");
+        string query = ConstructQuery(userName, userId, completedStatus, titleContains);
+
+        HttpResponseMessage response = await client.GetAsync("/todos"+query);
         string content = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
         {
@@ -38,5 +39,34 @@ public class TodoHttpClient : ITodoService
             PropertyNameCaseInsensitive = true
         })!;
         return todos;
+    }
+
+    private static string ConstructQuery(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = "";
+        if (!string.IsNullOrEmpty(userName))
+        {
+            query += $"?username={userName}";
+        }
+
+        if (userId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"userid={userId}";
+        }
+
+        if (completedStatus != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"completedstatus={completedStatus}";
+        }
+
+        if (!string.IsNullOrEmpty(titleContains))
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"titlecontains={titleContains}";
+        }
+
+        return query;
     }
 }
